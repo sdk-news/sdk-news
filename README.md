0c7aeec3cf3532fb207ed4f380f981fcd06c701b
victor.pavlychko <victor.pavlychko@grammarly.com>
2022-06-07 10:30:51 -0700

    Reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# SDK News
-
-https://sdk.news/
--- Sources/sdk-diff-tool/Crawler.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension ChangeSetStore {
-    func update() throws {
-        // this function is intentionally left blank to not abuse the documentation servers.
-    }
-}
